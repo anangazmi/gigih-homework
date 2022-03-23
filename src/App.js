b50af5f1acae51fc174a37a38f2551821ba3205b
Anang AZMI <azmiho9977@gmail.com>
2022-03-23 16:57:21 +0800

    react DOM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={data.album.images[1].url} alt="" />
+      <p>{data.album.name}</p>
+      <p>{data.album.artists[0].name}</p>
+      <button> Select </button>
     </div>
   );
 }
